9190fb962c0096eaee91eda05c4f34922e48327e
prabath1998 <prabhathudayanga1998@gmail.com>
2022-06-18 21:06:25 +0530

    Add configuration class

---
+++ src/main/java/com/example/jwt/config/SecurityConfiguration.java
@@ -0,0 +1,16 @@
+package com.example.jwt.config;
+
+import com.example.jwt.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserService userService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService);
+    }
+}
